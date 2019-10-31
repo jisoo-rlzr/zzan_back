6b0e43384500b62673cea2b86debd302e9f233f1
jisoo <jsoo744@gmail.com>
2019-11-01 00:05:23 +0900

    [*] Prisma Datamodel, Graphql Schema, Resolver 수정 및 서버에 반영

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+import { GraphQLServer } from "graphql-yoga"
+import resolvers from "./graphql/resolvers"
+import prisma from "./generated/prisma-client"
+
+const server = new GraphQLServer({
+    typeDefs: "src/graphql/schema.graphql",
+    resolvers,
+    context: {
+        prisma
+    }
+})
+
+server.start(() => console.log("Server is running on http://localhost:4000"))
